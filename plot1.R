2aad4597999d63321f70739e8c1fabbb17308fb3
Parkinson Zabelin <parkinson.zabelin@gmail.com>
2014-12-03 17:01:04 -0500

    new file:   plot1.R 	new file:   plot1.png 	new file:   plot2.R 	new file:   plot2.png 	new file:   plot3.R 	new file:   plot3.png 	new file:   plot4.R 	new file:   plot4.png

---
+++ plot1.R
@@ -0,0 +1,18 @@
+#The dataset("household_power_consumption.txt") in the current directory has 2,075,259 rows and 9 columns. 
+#We will only be using data from the dates 2007-02-01 and 2007-02-02.
+#Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+#Name each of the plot files as plot1.png, plot2.png, etc.
+
+  t1 <- as.Date("1/2/2007","%d/%m/%Y")		#Feb-01-2007
+  t2 <- as.Date("2/2/2007","%d/%m/%Y")		#Feb-02-2007
+  tb <- read.csv("household_power_consumption.txt",sep=";",colClasses=c(rep("character",2),rep("numeric",7)),na.strings="?")
+
+  tt <- as.Date(tb$Date,"%d/%m/%Y")
+  tb <- tb[which(tt == t1 | tt == t2),]		#select t1 and t2 only
+  tb <- tb[!is.na(tb[,5]),]			#remove NA
+
+  png(filename="plot1.png",width=480,height=480,bg="white")
+
+  hist(tb$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+
+  dev.off()
---
+++ plot3.R
@@ -0,0 +1,22 @@
+#The dataset("household_power_consumption.txt") in the current directory has 2,075,259 rows and 9 columns. 
+#We will only be using data from the dates 2007-02-01 and 2007-02-02.
+#Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels.
+#Name each of the plot files as plot1.png, plot2.png, etc.
+
+  t1 <- as.Date("1/2/2007","%d/%m/%Y")		#Feb-01-2007
+  t2 <- as.Date("2/2/2007","%d/%m/%Y")		#Feb-02-2007
+  tb <- read.csv("household_power_consumption.txt",sep=";",colClasses=c(rep("character",2),rep("numeric",7)),na.strings="?")
+
+  tt <- as.Date(tb$Date,"%d/%m/%Y")
+  tb <- tb[which(tt == t1 | tt == t2),]		#select t1 and t2 only
+  tb <- tb[!is.na(tb[,5]),]			#remove NA
+
+  png(filename="plot3.png",width=480,height=480,bg="white")
+
+  x <- strptime(paste(tb$Date,tb$Time),"%d/%m/%Y %H:%M:%S");
+  plot(x,tb$Sub_metering_1,ylab="Energy sub metering",type="l",xlab="")
+  lines(x,tb$Sub_metering_2,type="l",col="red")
+  lines(x,tb$Sub_metering_3,type="l",col="blue")
+  legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),pch="_",col=c("black","red","blue"))
+
+  dev.off()
